229b60eca1fee5d22bafb15d4bc609e53fb63642
vitamin <trysomevitamin@gmail.com>
2022-01-11 20:45:18 +0800

    non-Spring DI

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,19 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx  = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController con = (MyController) ctx.getBean("myController");
+		String greeting = con.sayHello();
+		System.out.println(greeting);
 	}
 
 }
